951e94545240368741056e8d022fb1b29c60396f
Nina <gadgetgeekette@hotmail.co.uk>
2024-05-28 11:47:54 +0100

    Setup databases and updated README

---
+++ sql-playground.sql
@@ -0,0 +1,17 @@
+\c nc_news
+
+-- \echo '\n *** DEV DB *** \n'
+
+SELECT * FROM articles;
+SELECT * FROM comments;
+SELECT * FROM articles;
+SELECT * FROM users;
+
+-- \c nc_news_test
+
+-- \echo '\n *** TEST DB *** \n'
+
+-- SELECT * FROM articles;
+-- SELECT * FROM comments;
+-- SELECT * FROM articles;
+-- SELECT * FROM users;
