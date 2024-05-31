e2e5cbf6366cd1bd2364521c7cc2914302d02fae
Nina <gadgetgeekette@hotmail.co.uk>
2024-05-31 12:22:47 +0100

    Added get user

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,9 +1,23 @@
-const {getUsersQuery} = require('../models/user-model');
+const {getUsersQuery, getUserQuery} = require('../models/user-model');
 
 exports.getUsers = (req, res, next) => {
     return getUsersQuery(next)
         .then((result) => {
             res.status(200).send({users: result});
+        })
+        .catch((err) => {
+            next(err);
+        });;
+};
+
+exports.getUser = (req, res, next) => {
+    if (!req.body.username || typeof req.body.username !== 'string') {
+        res.status(400).send({msg: 'Bad request'})
+    }
+
+    return getUserQuery(req.body.username, next)
+        .then((result) => {
+            res.status(200).send({user: result});
         })
         .catch((err) => {
             next(err);
